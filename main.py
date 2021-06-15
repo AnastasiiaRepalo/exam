80a319b65f208803989d52e8cdccf4fb5131ddb4
anastasiia <repalo.a@gmail.com>
2021-06-15 09:32:38 +0300

    initialized function

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def find_longest_subsequence(string):
+    two_last_letters = []
+    len_of_longest_subs = 0
+
+    for el in string:
+        asii_numbr = ord(el)
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    find_longest_subsequence('trial')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
